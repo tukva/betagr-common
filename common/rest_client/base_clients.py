1bfddaa356affc572b1e7f078873c3aaef9f549b
tukva <52979510+tukva@users.noreply.github.com>
2019-11-12 18:05:36 +0200

    Update base_clients.py

--- common/rest_client/base_clients.py
+++ common/rest_client/base_clients.py
@@ -92,3 +92,46 @@ class BaseClientSSO(BaseClient):
     async def reset_password(self, body):
         url = self.api_uris['reset_password']
         return await self.patch(url, body=body, cookies=self._cookies)
+
+
+class BaseClientParser(BaseClient):
+    def __init__(self, host, port, headers=None):
+        super().__init__(host, port, headers=headers)
+
+        self._api_uri = {
+            'all_teams': 'parse-links/teams',
+            'teams_by_link': 'parse-links/{link_id}/teams',
+            'real_teams': 'real-teams',
+        }
+
+    async def get_all_teams(self):
+        url = self._api_uri['all_teams']
+        async with await super().get(url, cookies=self._cookies, params=None) as response:
+            return await response.json()
+
+    async def get_teams_by_link(self, link_id):
+        url = self._api_uri['teams_by_link'].format(link_id=link_id)
+        async with await super().get(url, cookies=self._cookies, params=None) as response:
+            if response.status == 404:
+                return {"status": response.status}
+            return {"json": await response.json(), "status": response.status}
+
+    async def put_teams_by_link(self, link_id):
+        url = self._api_uri['teams_by_link'].format(link_id=link_id)
+        async with await super().put(url, cookies=self._cookies, params=None) as response:
+            return await response.text()
+
+    async def delete_teams_by_link(self, link_id):
+        url = self._api_uri['teams_by_link'].format(link_id=link_id)
+        async with await super().delete(url, cookies=self._cookies, params=None) as response:
+            return await response.text()
+
+    async def get_real_teams(self):
+        url = self._api_uri['real_teams']
+        async with await super().get(url, cookies=self._cookies, params=None) as response:
+            return await response.json()
+
+    async def put_real_teams(self):
+        url = self._api_uri['real_teams']
+        async with await super().put(url, cookies=self._cookies, params=None) as response:
+            return await response.text()
